82bbca4d2ff684bdc9f8fd22977da77dca157e5d
githubmave <joy-133@163.com>
2016-05-17 23:30:24 +1200

    my 3rd commit

--- app/routes.js
+++ app/routes.js
@@ -1,142 +1,55 @@
 var Todo = require('./models/todo');
 
-var aws=require('aws-sdk');
+function getTodos(res) {
+    Todo.find(function (err, todos) {
 
-aws.config.update({accessKeyId: 'AKIAIUQ5OWX24OGOLQ5A', secretAccessKey: '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'}); 
-
-
-aws.config.update({region: 'us-east-1'}); 
-
-
-var db = new aws.DynamoDB(); 
-
-var dbClient = new aws.DynamoDB.DocumentClient();
-
-dbClient.scan({ 
-        TableName : "CartItems_Tb", 
-        Limit : 50 
-        }, function(err, data) { 
-        if (err) { console.log(err); 
-        return; } 
-        console.log(data.Items); 
-        for (var ii in data.Items) { 
-        ii = data.Items[ii]; 
-        console.log(ii.NAME); 
-        console.log(ii.PRICE); 
-        console.log(ii.QUTY)
-        
-        } 
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err) {
+            res.send(err);
         }
-        ); 
 
-
-
-
-function getCartItmes(res) {
-
-     dbClient.scan({ 
-            TableName : "CartItems_Tb", 
-            Limit : 50 
-            }, function(err, data) { 
-                if (err) { console.log(err); 
-                return; } 
-
-
-                res.json(data.Items);
-                
-            }
-        ); 
-    
+        res.json(todos); // return all todos in JSON format
+    });
 }
 ;
 
 module.exports = function (app) {
 
     // api ---------------------------------------------------------------------
-    
-    //------------------------
-
-
-    app.get('/api/cartItemsLs', function (req, res) {
+    // get all todos
+    app.get('/api/todos', function (req, res) {
         // use mongoose to get all todos in the database
-        // getTodos(res);
-
-       getCartItmes(res);
-
-
+        getTodos(res);
     });
-
-
-
-
 
     // create todo and send back all todos after creation
-    app.post('/api/cartItemsLs', function (req, res) {
+    app.post('/api/todos', function (req, res) {
 
-       //------------------------------
+        // create a todo, information comes from AJAX request from Angular
+        Todo.create({
+            text: req.body.text,
+            done: false
+        }, function (err, todo) {
+            if (err)
+                res.send(err);
 
-       dbClient.put({
+            // get and return all the todos after you create another
+            getTodos(res);
+        });
 
-                TableName:"CartItems_Tb",
-                Item:{
-                "NAME": req.body.NAME,
-                "PRICE": req.body.PRICE,
-                "QUTY": req.body.QUTY
-
-               
-                      }
-           
-
-
-               }, function(err, data) {
-                    if (err) {
-                            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-                        } else {
-                            console.log("Added item:", JSON.stringify(data, null, 2));
-                        }
-            
-
-                      getCartItmes(res);
-                      
-               });
-      });
-
-
-
-    // delete a cartItem
-
-    app.delete('/api/cartItemsLs/:item_id', function (req, res){
-
-             dbClient.delete({
-
-                 _id: req.params.item_id,
-                TableName:"CartItems_Tb",
-                Key:{
-                  "NAME": req.params.item_id
-                // "PRICE": req.body.PRICE,
-                // "QUTY": req.body.QUTY
-
-               
-                    }
-           
-
-
-               }, function(err, data) {
-                    if (err) {
-                            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
-                        } else {
-                            console.log("Added item:", JSON.stringify(data, null, 2));
-                        }
-            
-
-                      getCartItmes(res);
-                      
-               });
-
-           
     });
 
+    // delete a todo
+    app.delete('/api/todos/:todo_id', function (req, res) {
+        Todo.remove({
+            _id: req.params.todo_id
+        }, function (err, todo) {
+            if (err)
+                res.send(err);
 
+            getTodos(res);
+        });
+    });
 
     // application -------------------------------------------------------------
     app.get('*', function (req, res) {
